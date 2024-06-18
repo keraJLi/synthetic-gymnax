8016966a951c9c80ffd75c8a306eec27531df437
jarek <jarek@bccn-berlin.de>
2024-06-18 12:29:42 +0200

    Added long description to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name="synthetic-gymnax",
     version="0.0.1",
@@ -8,6 +11,8 @@ setup(
     description="Synthetic gymnax environments",
     packages=find_packages(),
     include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[
         "chex",
         "gymnax",
